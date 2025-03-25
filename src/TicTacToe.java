fdef26f8d8bbb72ab4bcf6cc037ff1ae84ecca0c
mzerhusen <zerhusma@mail.uc.edu>
2025-03-24 23:24:22 -0400

    Final

--- src/TicTacToe.java
+++ src/TicTacToe.java
@@ -1,8 +1,10 @@
+import java.sql.SQLOutput;
 import java.util.Scanner;
 
 public class TicTacToe
 {
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
         Scanner input = new Scanner(System.in);
         String gameBoard[] = new String[9];
         String activePlayer = "X";
@@ -10,6 +12,8 @@ public class TicTacToe
         int playerSpace = 0;
         int turnCounter = 0;
         boolean validMove = false;
+        boolean gameWon = false;
+        boolean gameUnwinnable = false;
         boolean gameOver = false;
         boolean playAgain = false;
 
@@ -19,6 +23,12 @@ public class TicTacToe
             clearBoard(gameBoard);
             activePlayer = "X";
             turnCounter = 0;
+            validMove = false;
+            gameWon = false;
+            gameUnwinnable = false;
+            gameOver = false;
+            playAgain = false;
+
             do
             {
                 printBoard(gameBoard);
@@ -34,9 +44,15 @@ public class TicTacToe
                 gameBoard[playerSpace] = activePlayer;
                 turnCounter++;
 
+                //check for win
                 if (turnCounter > 4)
                 {
-                    gameOver = gameEndings(gameBoard, activePlayer);
+                    gameWon = gameEndings(gameBoard, activePlayer);
+                    gameUnwinnable = gameStalemate(gameBoard, activePlayer);
+                    if (gameWon || gameUnwinnable)
+                    {
+                        gameOver = true;
+                    }
                 }
 
                 if(!gameOver)
@@ -53,7 +69,18 @@ public class TicTacToe
             }
             while (!gameOver);
 
-            System.out.println("Player " + activePlayer + " has won!");
+            printBoard(gameBoard);
+
+            if(gameWon)
+            {
+                System.out.println("Player " + activePlayer + " has won!");
+            }
+            else if(gameUnwinnable)
+            {
+                System.out.println("There is no possible win. The game is a draw.");
+            }
+
+            playAgain = SafeInput.getYesNoConfirm(input, "Would you like to play again?");
         }
         while (playAgain);
     }
@@ -75,13 +102,22 @@ public class TicTacToe
             int leftColumn = (x * 3);
             int middleColumn = (x * 3) + 1;
             int rightColumn = (x * 3) + 2;
+            if(x == 0)
+            {
+                System.out.print("\n");
+            }
             System.out.printf("%1s|%1s|%1s\n", gameBoard[leftColumn], gameBoard[middleColumn], gameBoard[rightColumn]);
             if(x < 2)
             {
                 System.out.print("-+-+-\n");
             }
+            else
+            {
+                System.out.print("\n");
+            }
         }
     }
+
     private static boolean validMoveCheck(String[] gameBoard, int move)
     {
         boolean legalMove = false;
@@ -120,6 +156,191 @@ public class TicTacToe
                 gameEnded = true;
             }
         }
+
+        //check diagonals
+
+        if(gameBoard[0].equals(player) && gameBoard[4].equals(player) && gameBoard[8].equals(player))
+        {
+            gameEnded = true;
+        }
+        else if(gameBoard[2].equals(player) && gameBoard[4].equals(player) && gameBoard[6].equals(player))
+        {
+            gameEnded = true;
+        }
+
         return gameEnded;
+    }
+
+    private static boolean gameStalemate(String[] gameBoard, String player)
+    {
+        int xCount = 0;
+        int oCount = 0;
+        boolean rowDraw = false;
+        boolean columnDraw = false;
+        boolean leftDiagonalDraw = false;
+        boolean rightDiagonalDraw = false;
+        boolean gameDraw = false;
+
+        //Check for row win possibilities
+        for (int x = 0; x < 3; x++)
+        {
+            xCount = 0;
+            oCount = 0;
+
+            if(gameBoard[(x * 3)].equals("X"))
+            {
+                xCount++;
+            }
+            else if(gameBoard[x * 3].equals("O"))
+            {
+                oCount++;
+            }
+            if(gameBoard[(x * 3) + 1].equals("X"))
+            {
+                xCount++;
+            }
+            else if(gameBoard[(x * 3) + 1].equals("O"))
+            {
+                oCount++;
+            }
+            if(gameBoard[(x * 3) + 2].equals("X"))
+            {
+                xCount++;
+            }
+            else if(gameBoard[(x * 3) + 2].equals("O"))
+            {
+                oCount++;
+            }
+
+            if(oCount == 0 || xCount == 0)
+            {
+                rowDraw = false;
+                break;
+            }
+            else if(x == 2)
+            {
+                rowDraw = true;
+            }
+        }
+
+        //check for column win possibilities
+        for (int x = 0; x < 3; x++)
+        {
+            xCount = 0;
+            oCount = 0;
+
+            if(gameBoard[x].equals("X"))
+            {
+                xCount++;
+            }
+            else if(gameBoard[x].equals("O"))
+            {
+                oCount++;
+            }
+            if(gameBoard[(x + 3)].equals("X"))
+            {
+                xCount++;
+            }
+            else if(gameBoard[(x + 3)].equals("O"))
+            {
+                oCount++;
+            }
+            if(gameBoard[(x + 6)].equals("X"))
+            {
+                xCount++;
+            }
+            else if(gameBoard[(x + 6)].equals("O"))
+            {
+                oCount++;
+            }
+
+            if(oCount == 0 || xCount == 0)
+            {
+                columnDraw = false;
+                break;
+            }
+            else if(x == 2)
+            {
+                columnDraw = true;
+            }
+        }
+
+        //check for left diagonal draw
+        for (int x = 0; x < 1; x++)
+        {
+            xCount = 0;
+            oCount = 0;
+
+            if(gameBoard[0].equals("X"))
+            {
+                xCount++;
+            }
+            else if(gameBoard[0].equals("O"))
+            {
+                oCount++;
+            }
+            if(gameBoard[4].equals("X"))
+            {
+                xCount++;
+            }
+            else if(gameBoard[4].equals("O"))
+            {
+                oCount++;
+            }
+            if(gameBoard[8].equals("X"))
+            {
+                xCount++;
+            }
+            else if(gameBoard[8].equals("O"))
+            {
+                oCount++;
+            }
+
+            if(oCount == 0 || xCount == 0)
+            {
+                leftDiagonalDraw = false;
+            }
+            else
+            {
+                leftDiagonalDraw = true;
+            }
+        }
+
+        //check for right diagonal draw
+        for (int x = 0; x < 1; x++)
+        {
+            xCount = 0;
+            oCount = 0;
+
+            if (gameBoard[2].equals("X")) {
+                xCount++;
+            } else if (gameBoard[2].equals("O")) {
+                oCount++;
+            }
+            if (gameBoard[4].equals("X")) {
+                xCount++;
+            } else if (gameBoard[4].equals("O")) {
+                oCount++;
+            }
+            if (gameBoard[6].equals("X")) {
+                xCount++;
+            } else if (gameBoard[6].equals("O")) {
+                oCount++;
+            }
+
+            if (oCount == 0 || xCount == 0) {
+                rightDiagonalDraw = false;
+            } else {
+                rightDiagonalDraw = true;
+            }
+        }
+
+
+        if(rowDraw && columnDraw && leftDiagonalDraw && rightDiagonalDraw)
+        {
+            gameDraw = true;
+        }
+
+        return gameDraw;
     }
 }
